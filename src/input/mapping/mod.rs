18bf3710ff7a502f81270513e9a6c18eb7a1ab68
Uwe Klotz <uwe.klotz@gmail.com>
2023-05-29 21:45:15 +0200

    Add u7 to u14 conversions

--- src/input/mapping/mod.rs
+++ src/input/mapping/mod.rs
@@ -4,13 +4,30 @@
 pub mod korg_kaoss_dj;
 
 #[must_use]
+pub fn u7_be_to_u14(hi: u8, lo: u8) -> u16 {
+    u16::from(hi) << 7 | u16::from(lo)
+}
+
+#[must_use]
 pub fn u7_to_slider(input: u8) -> super::Slider {
     let position = f32::from(input) / 127.0;
     super::Slider { position }
 }
 
 #[must_use]
+pub fn u14_to_slider(input: u16) -> super::Slider {
+    let position = f32::from(input) / 16383.0;
+    super::Slider { position }
+}
+
+#[must_use]
 pub fn u7_to_center_slider(input: u8) -> super::CenterSlider {
     let position = f32::from(input) * 2.0 / 127.0 - 1.0;
+    super::CenterSlider { position }
+}
+
+#[must_use]
+pub fn u14_to_center_slider(input: u16) -> super::CenterSlider {
+    let position = f32::from(input) * 2.0 / 16383.0 - 1.0;
     super::CenterSlider { position }
 }
