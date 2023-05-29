2d796ef7874aa397d9066d63ae161800e39b3709
Uwe Klotz <uwe.klotz@gmail.com>
2023-05-29 23:49:27 +0200

    Convert u7 to slider encoder value

--- src/input/mapping/mod.rs
+++ src/input/mapping/mod.rs
@@ -15,6 +15,16 @@ pub fn u7_to_slider(input: u8) -> super::Slider {
 }
 
 #[must_use]
+pub fn u7_to_slider_encoder(input: u8) -> super::SliderEncoder {
+    let delta = if input < 64 {
+        f32::from(input) / 63.0
+    } else {
+        f32::from(input.wrapping_sub(128) as i8) / 64.0
+    };
+    super::SliderEncoder { delta }
+}
+
+#[must_use]
 pub fn u14_to_slider(input: u16) -> super::Slider {
     let position = f32::from(input) / 16383.0;
     super::Slider { position }
