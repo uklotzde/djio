b036a26eab4808c0e60c97957313fdd189f63afc
Uwe Klotz <uwe.klotz@gmail.com>
2023-06-01 12:33:41 +0200

    devices: Add missing feature gates

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -1,6 +1,11 @@
 // SPDX-FileCopyrightText: The djio authors
 // SPDX-License-Identifier: MPL-2.0
 
+#[cfg(feature = "midi")]
 pub mod korg_kaoss_dj;
-pub mod ni_traktor_s4mk3;
+
+#[cfg(feature = "midi")]
 pub mod pioneer_ddj_400;
+
+#[cfg(feature = "hid")]
+pub mod ni_traktor_s4mk3;
