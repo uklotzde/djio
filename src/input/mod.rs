7bb60d17f078a0f3c91c58d0b726681a2088e172
Uwe Klotz <uwe.klotz@gmail.com>
2023-06-07 12:13:44 +0200

    Fix test assertions

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -346,11 +346,11 @@ mod tests {
     #[test]
     #[allow(clippy::float_cmp)]
     fn pad_button_from_u7() {
-        debug_assert_eq!(
+        assert_eq!(
             PadButtonInput::MIN_PRESSURE,
             PadButtonInput::from_u7(0).pressure
         );
-        debug_assert_eq!(
+        assert_eq!(
             PadButtonInput::MAX_PRESSURE,
             PadButtonInput::from_u7(127).pressure
         );
@@ -359,11 +359,11 @@ mod tests {
     #[test]
     #[allow(clippy::float_cmp)]
     fn pad_button_from_u14() {
-        debug_assert_eq!(
+        assert_eq!(
             PadButtonInput::MIN_PRESSURE,
             PadButtonInput::from_u14(0).pressure
         );
-        debug_assert_eq!(
+        assert_eq!(
             PadButtonInput::MAX_PRESSURE,
             PadButtonInput::from_u14(16383).pressure
         );
@@ -372,28 +372,28 @@ mod tests {
     #[test]
     #[allow(clippy::float_cmp)]
     fn step_encoder_from_u7() {
-        debug_assert_eq!(0, StepEncoderInput::from_u7(0).delta);
-        debug_assert_eq!(1, StepEncoderInput::from_u7(1).delta);
-        debug_assert_eq!(63, StepEncoderInput::from_u7(63).delta);
-        debug_assert_eq!(-64, StepEncoderInput::from_u7(64).delta);
-        debug_assert_eq!(-1, StepEncoderInput::from_u7(127).delta);
+        assert_eq!(0, StepEncoderInput::from_u7(0).delta);
+        assert_eq!(1, StepEncoderInput::from_u7(1).delta);
+        assert_eq!(63, StepEncoderInput::from_u7(63).delta);
+        assert_eq!(-64, StepEncoderInput::from_u7(64).delta);
+        assert_eq!(-1, StepEncoderInput::from_u7(127).delta);
     }
 
     #[test]
     #[allow(clippy::float_cmp)]
     fn step_encoder_from_u14() {
-        debug_assert_eq!(0, StepEncoderInput::from_u14(0).delta);
-        debug_assert_eq!(1, StepEncoderInput::from_u14(1).delta);
-        debug_assert_eq!(8191, StepEncoderInput::from_u14(8191).delta);
-        debug_assert_eq!(-8192, StepEncoderInput::from_u14(8192).delta);
-        debug_assert_eq!(-1, StepEncoderInput::from_u14(16383).delta);
+        assert_eq!(0, StepEncoderInput::from_u14(0).delta);
+        assert_eq!(1, StepEncoderInput::from_u14(1).delta);
+        assert_eq!(8191, StepEncoderInput::from_u14(8191).delta);
+        assert_eq!(-8192, StepEncoderInput::from_u14(8192).delta);
+        assert_eq!(-1, StepEncoderInput::from_u14(16383).delta);
     }
 
     #[test]
     #[allow(clippy::float_cmp)]
     fn slider_from_u7() {
-        debug_assert_eq!(SliderInput::MIN_POSITION, SliderInput::from_u7(0).position);
-        debug_assert_eq!(
+        assert_eq!(SliderInput::MIN_POSITION, SliderInput::from_u7(0).position);
+        assert_eq!(
             SliderInput::MAX_POSITION,
             SliderInput::from_u7(127).position
         );
@@ -402,8 +402,8 @@ mod tests {
     #[test]
     #[allow(clippy::float_cmp)]
     fn slider_from_u14() {
-        debug_assert_eq!(SliderInput::MIN_POSITION, SliderInput::from_u14(0).position);
-        debug_assert_eq!(
+        assert_eq!(SliderInput::MIN_POSITION, SliderInput::from_u14(0).position);
+        assert_eq!(
             SliderInput::MAX_POSITION,
             SliderInput::from_u14(16383).position
         );
@@ -412,19 +412,19 @@ mod tests {
     #[test]
     #[allow(clippy::float_cmp)]
     fn center_slider_from_u7() {
-        debug_assert_eq!(
+        assert_eq!(
             CenterSliderInput::MIN_POSITION,
             CenterSliderInput::from_u7(0).position
         );
-        debug_assert!(CenterSliderInput::MIN_POSITION < CenterSliderInput::from_u7(1).position);
-        debug_assert!(CenterSliderInput::CENTER_POSITION > CenterSliderInput::from_u7(63).position);
-        debug_assert_eq!(
+        assert!(CenterSliderInput::MIN_POSITION < CenterSliderInput::from_u7(1).position);
+        assert!(CenterSliderInput::CENTER_POSITION > CenterSliderInput::from_u7(63).position);
+        assert_eq!(
             CenterSliderInput::CENTER_POSITION,
             CenterSliderInput::from_u7(64).position
         );
-        debug_assert!(CenterSliderInput::CENTER_POSITION < CenterSliderInput::from_u7(65).position);
-        debug_assert!(CenterSliderInput::MAX_POSITION > CenterSliderInput::from_u7(126).position);
-        debug_assert_eq!(
+        assert!(CenterSliderInput::CENTER_POSITION < CenterSliderInput::from_u7(65).position);
+        assert!(CenterSliderInput::MAX_POSITION > CenterSliderInput::from_u7(126).position);
+        assert_eq!(
             CenterSliderInput::MAX_POSITION,
             CenterSliderInput::from_u7(127).position
         );
@@ -433,25 +433,19 @@ mod tests {
     #[test]
     #[allow(clippy::float_cmp)]
     fn center_slider_from_u14() {
-        debug_assert_eq!(
+        assert_eq!(
             CenterSliderInput::MIN_POSITION,
             CenterSliderInput::from_u14(0).position
         );
-        debug_assert!(CenterSliderInput::MIN_POSITION < CenterSliderInput::from_u14(1).position);
-        debug_assert!(
-            CenterSliderInput::CENTER_POSITION > CenterSliderInput::from_u14(8191).position
-        );
-        debug_assert_eq!(
+        assert!(CenterSliderInput::MIN_POSITION < CenterSliderInput::from_u14(1).position);
+        assert!(CenterSliderInput::CENTER_POSITION > CenterSliderInput::from_u14(8191).position);
+        assert_eq!(
             CenterSliderInput::CENTER_POSITION,
             CenterSliderInput::from_u14(8192).position
         );
-        debug_assert!(
-            CenterSliderInput::CENTER_POSITION < CenterSliderInput::from_u14(8193).position
-        );
-        debug_assert!(
-            CenterSliderInput::MAX_POSITION > CenterSliderInput::from_u14(16382).position
-        );
-        debug_assert_eq!(
+        assert!(CenterSliderInput::CENTER_POSITION < CenterSliderInput::from_u14(8193).position);
+        assert!(CenterSliderInput::MAX_POSITION > CenterSliderInput::from_u14(16382).position);
+        assert_eq!(
             CenterSliderInput::MAX_POSITION,
             CenterSliderInput::from_u14(16383).position
         );
@@ -460,12 +454,12 @@ mod tests {
     #[test]
     #[allow(clippy::float_cmp)]
     fn slider_encoder_from_u7() {
-        debug_assert_eq!(0.0, SliderEncoderInput::from_u7(0).delta);
-        debug_assert_eq!(
+        assert_eq!(0.0, SliderEncoderInput::from_u7(0).delta);
+        assert_eq!(
             SliderEncoderInput::DELTA_PER_CW_REV,
             SliderEncoderInput::from_u7(63).delta
         );
-        debug_assert_eq!(
+        assert_eq!(
             SliderEncoderInput::DELTA_PER_CCW_REV,
             SliderEncoderInput::from_u7(64).delta
         );
@@ -474,12 +468,12 @@ mod tests {
     #[test]
     #[allow(clippy::float_cmp)]
     fn slider_encoder_from_u14() {
-        debug_assert_eq!(0.0, SliderEncoderInput::from_u14(0).delta);
-        debug_assert_eq!(
+        assert_eq!(0.0, SliderEncoderInput::from_u14(0).delta);
+        assert_eq!(
             SliderEncoderInput::DELTA_PER_CW_REV,
             SliderEncoderInput::from_u14(8191).delta
         );
-        debug_assert_eq!(
+        assert_eq!(
             SliderEncoderInput::DELTA_PER_CCW_REV,
             SliderEncoderInput::from_u14(8192).delta
         );
